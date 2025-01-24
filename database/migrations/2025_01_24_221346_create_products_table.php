9cf1bc4ef91154a26d7a65fe211a070de6900138
Khorram Khondkar <khorramk.kbsk@gmail.com>
2025-01-24 22:56:50 +0000

    Product table created

---
+++ database/migrations/2025_01_24_221346_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('price')->nullable();
+            $table->longText('description')->nullable();
+            $table->float('sale_price')->nullable();
+            $table->string('product_image_path')->nullable();
+            $table->unsignedBigInteger('product_categories_id');
+            $table->boolean('active')->nullable();
+            $table->foreign('product_categories_id')->references('id')->on('product_categories')->onDelete('cascade')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
