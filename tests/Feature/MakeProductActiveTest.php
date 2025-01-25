c187a0d431f1a7856b12b79d591f6cd342ddb869
Khorram Khondkar <khorramk.kbsk@gmail.com>
2025-01-25 19:37:59 +0000

    Adjusting the test for logic

--- tests/Feature/MakeProductActiveTest.php
+++ tests/Feature/MakeProductActiveTest.php
@@ -19,20 +19,20 @@ class MakeProductActiveTest extends TestCase
 
        $productCategory = new ProductCategory();
 
-       $productCategory->name = 'T-shirt';
+       $productCategory->name = 'Socks';
        $productCategory->category_link = 'https://cuttlefish.com/products/categories/clothes';
 
        $productCategory->save();
        $productCategory->products()->create([
-        'name' => 'Bliss shirt',
+        'name' => 'Fluffy Socks',
         'price' => 0.99,
         'active' => 1
-    ]);
-//       $productCategory->attach($product);
+       ]);
+
         $message = 'The follwing product is inactive: '. (string) $productCategory->products()->first()->name;
 
-         $this->artisan('make:product:inactive')
-                        ->expectsChoice('Please choose product Categories name', 'T-shirt', ['Pants', 'Tie', 'Socks', 'Suit', 'Coat', 'T-shirt'])
+        $this->artisan('make:product:inactive')
+                        ->expectsChoice('Please choose product Categories name', 'Socks', ['Pants', 'Tie', 'Socks', 'Suit', 'Coat', 'T-shirt'])
                         ->doesntExpectOutput('Successfull')
                         ->expectsOutput($message);
 
